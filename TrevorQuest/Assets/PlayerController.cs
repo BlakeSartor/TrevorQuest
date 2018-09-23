7b2a4a601bf37eaa9dee1b3fe12e8e7f6b2706dd
BlakeSartor <blakesartor@gmail.com>
2018-09-23 00:05:22 -0500

    BIG CHANGES

---
+++ TrevorQuest/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Camera cam;
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // move player to mouse click
+                Debug.Log("We hit" + hit.collider.name + " " + hit.point);
+                // stop focusing objects
+            }
+        }
+	}
+}
