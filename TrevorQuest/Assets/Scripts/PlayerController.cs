aa28a58ce5eb68ff751c9a15b0f08aa7b90ce09c
BlakeSartor <blakesartor@gmail.com>
2018-09-23 00:39:08 -0500

    Movement added

---
+++ TrevorQuest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerEngine))]
+public class PlayerController : MonoBehaviour {
+    Camera cam;
+    PlayerEngine engine;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        engine = GetComponent<PlayerEngine>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // move player to mouse click
+                engine.MoveToPoint(hit.point);
+                Debug.Log("We hit" + hit.collider.name + " " + hit.point);
+                // stop focusing objects
+            }
+        }
+	}
+}
---
+++ TrevorQuest/Assets/Scripts/PlayerEngine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class PlayerEngine : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+
+	}
+
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+}
