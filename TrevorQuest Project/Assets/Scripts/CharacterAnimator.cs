f9cd192b91933cce9a77d47168dc1f62d417cc41
BlakeSartor <blakesartor@gmail.com>
2018-09-25 04:09:36 -0500

    Added Model
    Added character model with idle/run/walk animation.

---
+++ TrevorQuest Project/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    const float locomotionAnimationSmoothTime = .1f;
+
+    Animator animator;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+
+	}
+}
